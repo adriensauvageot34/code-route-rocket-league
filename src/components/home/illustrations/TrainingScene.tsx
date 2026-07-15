2359d4a10b0e55a490c17eab15ffa4f99321c036
adriensauvageot34 <adrien.sauvageot34@gmail.com>
2026-07-15 11:29:30 +0200

    Add TrainingScene.tsx

---
+++ src/components/home/illustrations/TrainingScene.tsx
@@ -0,0 +1,32 @@
+import { SceneGroup, SceneLayer } from "@/components/home/illustrations/SceneGroup";
+import { homeIllustrationAssets } from "@/lib/home/homeIllustrationAssets";
+
+const assets = homeIllustrationAssets.training;
+
+export function TrainingScene() {
+  return (
+    <div className="home-scene training-scene" data-scene="training">
+      <SceneGroup depth="background" layer={0} name="decor">
+        <SceneLayer asset={assets.background} preload />
+      </SceneGroup>
+
+      <SceneGroup depth="distant" layer={1} name="analysis-distant-cars">
+        <SceneLayer asset={assets.distantCarsOcclusion} />
+      </SceneGroup>
+
+      <SceneGroup depth="focal" layer={2} name="ball">
+        <SceneLayer asset={assets.ball} />
+        <SceneLayer asset={assets.ballEnergyGold} />
+      </SceneGroup>
+
+      <SceneGroup depth="fennec" layer={3} name="fennec">
+        <SceneLayer asset={assets.fennecBase} />
+        <SceneLayer asset={assets.lightsVioletGlow} />
+      </SceneGroup>
+
+      <SceneGroup depth="foreground" future layer={4} name="transition">
+        <SceneLayer asset={assets.transitionWaveGold} />
+      </SceneGroup>
+    </div>
+  );
+}
